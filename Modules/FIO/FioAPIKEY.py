4afae5e0faccf5bc8c11f9145772288dc959b62b
bRoss8989 <brbhockey88@aol.com>
2023-06-19 23:03:34 +0000

    fioapikey

---
+++ Modules/FIO/FioAPIKEY.py
@@ -0,0 +1,14 @@
+import json
+import requests
+
+def FIO_APIKEY():
+    with open('auth.json', 'r') as file:
+        apiauth = json.load(file)
+        
+    FIOAPIKEY = requests.post('https://rest.fnar.net/auth/login',json=apiauth)
+    
+    if str(FIOAPIKEY) != '<Response [200]>':
+        return "auth error"
+    else:
+        FIOAPIKEY = json.loads(FIOAPIKEY.content)
+        return FIOAPIKEY['AuthToken']
