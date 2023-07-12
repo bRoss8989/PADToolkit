7808db199bfdc670aaf4e5acbf2ff59ccd43ebf3
bRoss8989 <brbhockey88@aol.com>
2023-07-12 05:39:56 +0000

    write

---
+++ Modules/Write/WriteRecipes.py
@@ -0,0 +1,15 @@
+from Modules.FIO.FioRecipesDict import FioRecipesDict
+from Modules.FIO.FioMaterialsDict import FioMaterialsDict
+from Modules.Storage.StoreMongo import StoreMongo
+
+def BuildRecipes(version):
+    fio_recipes = FioRecipesDict()
+    fio_mat = FioMaterialsDict()
+    re = ['minerals','gases','ores','liquids']
+    
+    for key, value in fio_mat.items():
+        if value['CategoryName'] in re:
+            fio_recipes['=>'+key] = {'RecipeName': '=>'+key, 'Inputs': [], 'Outputs': [{'Ticker': key, 'Amount': 0}]}
+    
+    return StoreMongo('PAD',1, version, {'recipes':fio_recipes})
+    
