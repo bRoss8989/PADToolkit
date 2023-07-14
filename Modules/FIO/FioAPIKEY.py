6a612cbcbc25a0c93dbc0b6718bdd340bde69157
bRoss8989 <brbhockey88@aol.com>
2023-07-14 03:07:54 +0000

    apifix

--- Modules/FIO/FioAPIKEY.py
+++ Modules/FIO/FioAPIKEY.py
@@ -8,7 +8,7 @@ def FIO_APIKEY():
         
     FIOAPIKEY = requests.post('https://rest.fnar.net/auth/login',json=apiauth)
     
-    if FIOAPIKEY.status.code != 200:
+    if FIOAPIKEY.status_code != 200:
         return "failed to get key"
     else:
         FIOAPIKEY = json.loads(FIOAPIKEY.content)
