be4218a040320adf959037214afae204c59f8cd8
bRoss8989 <brbhockey88@aol.com>
2023-07-29 18:11:01 +0000

    jumpcalc

--- GlobalVars.py
+++ GlobalVars.py
@@ -1,3 +1,10 @@
+from Modules.Transformers.StaticSystemConns import system_conns
+from Modules.Transformers.Parsec import parsec
+from Modules.FIO.FioNaturalSystems import FioNaturalSystemsList
 
+
+conns = system_conns()
+systems = FioNaturalSystemsList()
+parsec_dict = parsec()
 
 
--- Modules/Write/StaticData.py
+++ Modules/Write/StaticData.py
@@ -1,12 +1,19 @@
 from Modules.Storage.StoreMongo import StoreMongo
 from Modules.Transformers.StaticRecipes import StaticRecipes
 from Modules.Transformers.T2_StaticRecipeOutputs import T2_StaticRecipeOutputs
+from Modules.FIO.FioNaturalSystems import FioNaturalSystemsList
+from Modules.FIO.FioNaturalPlanets import FioNaturalPlanetsList
 
 def StaticDataCreate(version):
 
     # tier 1
 
     fio_recipes = StaticRecipes()
+
+    systems = FioNaturalSystemsList()
+
+    crafting_planets = FioNaturalPlanetsList()
+    delivery_planets = FioNaturalPlanetsList(cx=1)
 
     # tier 2 (dependant on t1)
 
@@ -15,5 +22,7 @@ def StaticDataCreate(version):
     # write to mongo
 
     StoreMongo('PAD',1, version, {'recipes':fio_recipes,
-                                 'recipe_outputs':outputs})
+                                 'recipe_outputs':outputs,
+                                 'crafting_planets':crafting_planets,
+                                 'delivery_planets':delivery_planets})
     
