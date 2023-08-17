6c1bb032abb8e165c0ecb40a477d93c52fa8bea0
bRoss8989 <brbhockey88@aol.com>
2023-08-17 20:24:36 +0000

    recipe-fix

--- Modules/FIO/FioRecipesDict.py
+++ Modules/FIO/FioRecipesDict.py
@@ -6,11 +6,13 @@ def FioRecipesDict():
 
     recipes_dict = {}
 
+    old = ['75xPG=>1xBDE', '100xPE 1xAR 1xTHF=>20xINS', '20xH2O 1xDDT 2xSOI=>10xPIB', '40xH2O 2xDDT 4xSOI=>15xHOP', '30xH2O 1xDDT 3xSOI=>5xGRA']
+
     for fiorecipe in recipes_list:        
 
         recipe = fiorecipe['RecipeName']
         
-        if recipe == '=>':
+        if recipe == '=>' or recipe in old:
             continue
         
         recipes_dict[recipe] = fiorecipe
