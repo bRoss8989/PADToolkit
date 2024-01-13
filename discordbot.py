756e5e363b605ae8ffd88769efe3f96181e548a4
bRoss8989 <brbhockey88@aol.com>
2024-01-13 05:37:07 +0000

    discordbot

--- discordbot.py
+++ discordbot.py
@@ -1,6 +1,8 @@
 import discord
 import asyncio
+import pandas as pd
 from Modules.Discord.discordpass import DISCORD_TOKEN
+from Modules.Discord.rr_msg import rr_msg
 
 intents = discord.Intents.default()
 client = discord.Client(intents=intents)
@@ -18,11 +20,28 @@ async def on_ready():
     for guild in client.guilds:
         for channel in guild.channels:
             
-            if channel.name == 'test3' and isinstance(channel, discord.TextChannel):
-                await channel.send('Bot here')
+            if channel.name == 'rr-livedata' and isinstance(channel, discord.TextChannel):
+                
+                ff, he3, h = rr_msg()
+                
+                ff_df = pd.DataFrame(ff)
+                he3_df = pd.DataFrame(he3)
+                h_df = pd.DataFrame(h)
 
-            if channel.name == 'general' and isinstance(channel, discord.TextChannel):
-                await channel.send('Bot here')            
+                md1 = ff_df.to_markdown()
+                md2 = he3_df.to_markdown()
+                md3 = h_df.to_markdown()
+
+                discord_message = (
+                    f"VH-778b REF base summary\n```\n{md1}\n```\n"
+                    f"VH-043e HE3 base summary\n```\n{md2}\n```\n"
+                    f"Hydron H base summary\n```\n{md3}\n```"
+                )
+                
+                await channel.send(discord_message)
+
+#            if channel.name == 'hcc-livedata' and isinstance(channel, discord.TextChannel):
+#                await channel.send('Bot here')            
 
 
 client.run(DISCORD_TOKEN)
